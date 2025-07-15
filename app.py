95e79bfe38c199da193f321c90bd130dbc8a9917
Manish Thakur <manishthakur272003@gmail.com>
2025-07-15 21:06:30 +0530

    data tranformation

--- app.py
+++ app.py
@@ -3,13 +3,19 @@ from src.datascience.exception import CustomException
 import sys 
 from src.datascience.components.data_ingestion import dataingestion
 from src.datascience.components.data_ingestion import dataingestionconfig
+from src.datascience.components.data_transformation import DataTransformationConfig,DataTransformation
 if __name__=="__main__":
      logging.info("the excution is done")
 
      try:
         # data_ingestion=dataingestionconfig()
          data_ingestion=dataingestion() 
-         data_ingestion.initiate_data_ingestion()
+         train_data_path,test_data_path,=data_ingestion.initiate_data_ingestion()
+         Data_Transformation_Config=DataTransformationConfig()
+         Data_transformation=DataTransformation()
+         Data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+
+
      except Exception as e:
         logging.info("Custom Exception")
         raise CustomException(e,sys)
