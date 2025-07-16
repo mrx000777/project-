fbe4deb05657f3d3d69f802724fca57349e95641
Manish Thakur <manishthakur272003@gmail.com>
2025-07-17 00:50:21 +0530

    model trainer

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ import sys
 from src.datascience.components.data_ingestion import dataingestion
 from src.datascience.components.data_ingestion import dataingestionconfig
 from src.datascience.components.data_transformation import DataTransformationConfig,DataTransformation
+from src.datascience.components.model_trainer import ModelTrainerConfig,ModelTrainer
+from src.datascience.utils import evaluate_models
+
 if __name__=="__main__":
      logging.info("the excution is done")
 
@@ -13,7 +16,9 @@ if __name__=="__main__":
          train_data_path,test_data_path,=data_ingestion.initiate_data_ingestion()
          Data_Transformation_Config=DataTransformationConfig()
          Data_transformation=DataTransformation()
-         Data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+         train_arr,test_arr,_= Data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+         model_trainer=ModelTrainer()
+         print(model_trainer.initiate_model_trainer(train_arr,test_arr))
 
 
      except Exception as e:
--- src/datascience/utils.py
+++ src/datascience/utils.py
@@ -6,6 +6,8 @@ import pandas as pd
 from dotenv import load_dotenv
 import pymysql
 import pickle
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import r2_score
 import numpy as np 
 load_dotenv()
 host=os.getenv("host")
@@ -39,3 +41,34 @@ def save_object(file_path, obj):
 
     except Exception as e:
         raise CustomException(e, sys)
+def evaluate_models(x_train, y_train,x_test,y_test,models,param):
+     try:
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(x_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(x_train,y_train)
+
+            #model.fit(X_train, y_train)  # Train model
+
+            y_train_pred = model.predict(x_train)
+
+            y_test_pred = model.predict(x_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+
+     except Exception as e:
+        raise CustomException(e, sys)
+print("evaluate_models loaded!")
